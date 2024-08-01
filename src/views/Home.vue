8ba914d77cd30457075311cd9d2ee9d07b2164a8
yu <2665764951@qq.com>
2024-08-01 09:19:31 +0800

    add `Home`

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import router from '@/router'
+
+const handleRouter = function (page: string) {
+  router.push({ name: page })
+}
+</script>
+
+<template>
+  <h1 class="bg-sky-400">Home</h1>
+</template>
+
+<style scoped>
+@import 'tailwindcss/tailwind.css';
+</style>
