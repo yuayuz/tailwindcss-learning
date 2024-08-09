b7707ecb26242f0e4af2260bb1f619565c24790b
yu <2665764951@qq.com>
2024-08-09 23:41:28 +0800

    add `Stroke`

---
+++ src/views/SVG/Stroke.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HeaderH1 from '@/components/HeaderH1.vue'
+import ParagraphH2 from '@/components/ParagraphH2.vue'
+</script>
+
+<template>
+  <header-h1>Stroke</header-h1>
+  <!-- 用于设置 SVG 元素笔触样式的实用程序 -->
+  <paragraph-h2>设置描边颜色</paragraph-h2>
+  <!-- 使用 stroke-* 实用程序来改变 SVG 的描边颜色 -->
+  <svg class="stroke-cyan-500">
+    <!-- ... -->
+  </svg>
+</template>
+
+<style scoped></style>
