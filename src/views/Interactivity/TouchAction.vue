d291d7765a9bc32468544408fc546f30ed759914
yu <2665764951@qq.com>
2024-08-09 21:41:30 +0800

    add `TouchAction`

---
+++ src/views/Interactivity/TouchAction.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeaderH1 from '@/components/HeaderH1.vue'
+import ParagraphH2 from '@/components/ParagraphH2.vue'
+</script>
+
+<template>
+  <header-h1>Touch Action</header-h1>
+  <!-- 用于控制元素如何在触摸屏上滚动和缩放的实用程序 -->
+  <paragraph-h2>设置触摸动作</paragraph-h2>
+  <!-- 使用 touch-* 实用程序来控制元素在触摸屏上的滚动（平移）和缩放（捏合） -->
+</template>
+
+<style scoped></style>
